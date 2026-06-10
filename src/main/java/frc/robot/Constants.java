57cfa8ffdb1a4dee988aa952d905c226f8d86986
Kfir <kfir.rousso@gmail.com>
2026-06-10 15:17:30 -0600

    Added configs for IntakeSubsystem, might not work though, If it aint broke dont fix it - Mooshall

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -202,6 +202,10 @@ public final class Constants {
   public static final class IntakeConstants {
         public static final int INTAKE_MOTOR_ID = 0; //TODO: correct this once krillpler is built
         public static final float INTAKING_POWER = 1; 
+        public static final int LEADER_FORWARD_CHANNEL = 0;
+        public static final int FOLLOWER_FORWARD_CHANNEL = 0;
+        public static final int LEADER_REVERSE_CHANNEL = 1;
+        public static final int FOLLOWER_REVERSE_CHANNEL = 1;
         
     }
 
