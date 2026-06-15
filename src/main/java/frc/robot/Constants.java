c1980b11ae26c6989036a1055bcfc671d8f24096
MOoshalll <Mbkturner@gmail.com>
2026-06-15 09:19:00 -0600

    copied liamd hopper code into this branch

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -257,13 +257,14 @@ public final class Constants {
     //this is nathans code, yet to be tested
 
     public static final class HopperConstants {
-        public static final int TOP_ROLLERS_ID = 0;
-        public static final int BELTS_ID = 0;
-        public static final int KICKER_ID = 0;
-        //TODO: correct these once krillpler is wired
+      public static final double HOPPER_SPEED = 0.5;
+      public static final double BELT_SPEED = 0.5;
+      public static final double OUTTAKE_HOPPER_SPEED = -0.5;
 
-       
-        
+      public static final int TOP_ROLLERS_ID = 17;
+      public static final int TOP_BELTS_ID = 18;
+      public static final int BOTTOM_BELTS_ID = 20;
+      public static final int INDEXER_ID = 19;
     }
 
     public static final class TurretConstants {
--- src/main/java/frc/robot/subsystems/HopperSubsystem.java
+++ src/main/java/frc/robot/subsystems/HopperSubsystem.java
@@ -1,21 +1,74 @@
-package frc.robot.subsystems;
+package frc.robot.subsystems; // Marshal lmk if this is fine.
+
+import edu.wpi.first.wpilibj2.command.Command;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.HopperConstants;
+import frc.robot.configs.HopperConfig;
+
 public class HopperSubsystem extends SubsystemBase{
-    private final WPI_TalonSRX mToproller;
-    private final WPI_TalonSRX mBelts;
-    private final WPI_TalonSRX mKicker;
 
+    private final WPI_TalonSRX mTopRoller;
+    private final WPI_TalonSRX mTopBelt;
+    private final WPI_TalonSRX mIndexer;
+    private final WPI_TalonSRX mBottomBelt;
     
-    
+    public boolean isShooting = false; // Just to track if we are shooting or not just in case. 
+    // Currently not in use.
+     
+
     public HopperSubsystem(){
+        
+        mTopRoller = new WPI_TalonSRX(HopperConstants.TOP_ROLLERS_ID);
+        mBottomBelt = new WPI_TalonSRX(HopperConstants.BOTTOM_BELTS_ID);
+        mTopBelt = new WPI_TalonSRX(HopperConstants.TOP_BELTS_ID);
+        mIndexer = new WPI_TalonSRX(HopperConstants.INDEXER_ID);
 
-    
-    
-        mToproller = new WPI_TalonSRX(HopperConstants.TOP_ROLLERS_ID);
-        mBelts = new WPI_TalonSRX(HopperConstants.BELTS_ID);
-        mKicker = new WPI_TalonSRX(HopperConstants.KICKER_ID);
+        HopperConfig.configure(mTopRoller);
+        HopperConfig.configure(mBottomBelt);
+        HopperConfig.configure(mTopBelt);
+        HopperConfig.configure(mIndexer);
     } 
 
+    public void runTopRoller(double speed) {
+        mTopRoller.set(speed);
+    }
+
+    public void runShoot(double speed) {
+        mTopBelt.set(speed);
+        mBottomBelt.set(speed);
+        mIndexer.set(speed);
+    }
+
+    public void stopIntake() {
+        runTopRoller(0);
+        runShoot(0);
+    }
+
+    public Command runIntakeCommand() { // Moodshal said this can have problemss. 
+        return run(
+        () -> {
+            runTopRoller(HopperConstants.HOPPER_SPEED);
+              });
+    }
+
+    public Command runShootCommand() {
+        return run(
+            () -> {
+                runShoot(HopperConstants.HOPPER_SPEED);
+            });
+    }
+
+    public Command stopIntakeCommand() {
+        return run(
+            () -> {
+                stopIntake();
+            });
+    }
+
+    @Override
+    public void periodic() {
+    }
+
 }
+
