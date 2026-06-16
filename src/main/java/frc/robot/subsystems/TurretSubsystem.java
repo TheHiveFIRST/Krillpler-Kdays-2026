415a9cc423e22b660675a224f71b2c8ff8d7e099
MOoshalll <Mbkturner@gmail.com>
2026-06-16 14:53:29 -0600

    created methods for turretsubsystem: turnt ot a target, and manual controls. also a boolean for determining if the turret is on target

--- src/main/java/frc/robot/subsystems/TurretSubsystem.java
+++ src/main/java/frc/robot/subsystems/TurretSubsystem.java
@@ -2,17 +2,27 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.controls.PositionVoltage;
+import com.ctre.phoenix6.controls.VelocityVoltage;
 
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.units.Units;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.TurretConstants;
 import frc.robot.configs.TurretConfig;
 public class TurretSubsystem extends SubsystemBase{
     TalonFX mTurretMotor = new TalonFX(TurretConstants.TURRET_MOTOR_ID);
-    
+    double TurretMotorTarget = 0;
+    public static boolean turretAtTarget = false;
+    public static boolean turretEnabled = true;
+    PositionVoltage turnPositionRequest = new PositionVoltage(0);
+    VelocityVoltage manualVelocityRequest = new VelocityVoltage(0);
     public TurretSubsystem() {
     
         mTurretMotor.getConfigurator().apply(TurretConfig.trackHub);
+        
 
 
 
@@ -24,5 +34,38 @@ public class TurretSubsystem extends SubsystemBase{
         return safeMotorTarget;
     }
     
+    public void setTurnSetpoint(double position, AngularVelocity velocity) {
+        mTurretMotor.setControl(turnPositionRequest.withPosition(position).withVelocity(velocity));
+    }
+
+    public void setTurretTarget(Rotation2d TurretTarget){
+        setTurnSetpoint(getMotorTarget(TurretTarget), Units.RotationsPerSecond.of(0));
+        TurretMotorTarget = getMotorTarget(TurretTarget);
+
+    }
+    
+    public boolean isTurretAtTarget() {
+        return (Math.abs(mTurretMotor.getPosition().getValueAsDouble() - TurretMotorTarget) < 0.05);
+    }
+    
+    //Manual turret controls
+    public void runTurretClockwise() {
+        mTurretMotor.setControl(manualVelocityRequest.withVelocity(5));
+    }
+
+    public void runTurretCounterClockwise() {
+        mTurretMotor.setControl(manualVelocityRequest.withVelocity(-5));
+    }
+
+    public void stopTurret() {
+        mTurretMotor.stopMotor();
+    }
+
+    public void periodic() {
+        turretAtTarget = isTurretAtTarget();
+    }
+    
+
+
 
 }
