b7b5ab7a90296dc4faa4e73a2ac2ff89d6863951
MOoshalll <Mbkturner@gmail.com>
2026-07-09 18:02:16 -0600

    fixed swerve wheel offset values

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -33,11 +33,11 @@ public final class Constants {
 
   public static final class DriveConstants{ 
     //allowed max speeds
-    public static final double MAX_SPEED_METERS_PER_SECOND = 9.6; 
+    public static final double MAX_SPEED_METERS_PER_SECOND = 4.8; 
     public static final double MAX_ANGULAR_SPEED =  2 * Math.PI; // rad/s 
     //Chassis config - width, depth, CAN IDS and angular offset values in Designdoc.md
   
-    public static final double WHEEL_CENTER_WIDTH = Units.inchesToMeters(12.25);
+    public static final double WHEEL_CENTER_WIDTH = Units.inchesToMeters(12.75);
     // Distance between centers of right and left wheels on robot
     public static final double WHEEL_CENTER_DEPTH = Units.inchesToMeters(10.25);
     // Distance between front and back wheels on robot
